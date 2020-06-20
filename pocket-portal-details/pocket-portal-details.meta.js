9327471eba5d49a271cbfacf53ec3285268c86f8
Loskir <kl@sinker.ru>
2020-06-20 15:54:37 +0300

    Add pocket portal details

---
+++ pocket-portal-details/pocket-portal-details.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @author         Zaso x Loskir
+// @id             iitc-plugin-pocket-portal-details
+// @name           IITC plugin: Pocket Portal Details
+// @category       Portal Info
+// @version        0.0.5
+// @updateURL      https://raw.githubusercontent.com/Loskir/iitc-plugins/master/pocket-portal-details/pocket-portal-details.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Loskir/iitc-plugins/master/pocket-portal-details/pocket-portal-details.user.js
+// @description    Append a pocket box containing some restricted details of the selected portal.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
