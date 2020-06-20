4951460f953a67bb1a3041c622640124f2464d5c
Loskir <kl@sinker.ru>
2020-06-20 16:54:14 +0300

    Add dt replace portal

---
+++ dt-replace-portal/dt-replace-portal.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id             iitc-plugin-dt-replace-portal@Loskir
+// @name           IITC plugin: Draw Tools Replace Portal
+// @description    Quickly replace one portal with another in Draw Tools
+// @category       Info
+// @version        1.0.2
+// @updateURL      https://raw.githubusercontent.com/Loskir/iitc-plugins/master/dt-replace-portal/dt-replace-portal.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Loskir/iitc-plugins/master/dt-replace-portal/dt-replace-portal.user.js
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
---
+++ dt-replace-portal/readme.md
@@ -0,0 +1,7 @@
+# Draw Tools Replace Portal
+
+![Demo](demo.gif)
+
+Easily move all the links you drew from one portal to another. Great for creating plans.
+
+My own plugin.
